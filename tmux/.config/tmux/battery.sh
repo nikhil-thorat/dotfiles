19c2eb8500890e2dd7e76dbefee7e9ffe6b602dc
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-07-26 21:30:51 +0530

    changed colors

--- tmux/.config/tmux/battery.sh
+++ tmux/.config/tmux/battery.sh
@@ -5,14 +5,14 @@ percentage=$(cat /sys/class/power_supply/BAT0/capacity)
 
 if [ "$status" = "Discharging" ]; then
     if [ "$percentage" -ge 75 ]; then
-        echo "#[fg=brightgreen,bold,bg=default] λ "
+        echo "#[fg=green,bold,bg=default] λ "
     elif [ "$percentage" -ge 50 ]; then
-        echo "#[fg=brightblue,bold,bg=default] λ "
+        echo "#[fg=blue,bold,bg=default] λ "
     elif [ "$percentage" -ge 25 ]; then
-        echo "#[fg=brightyellow,bold,bg=default] λ "
+        echo "#[fg=pink,bold,bg=default] λ "
     else
-        echo "#[fg=brightred,bold,bg=default] λ "
+        echo "#[fg=cyan,bold,bg=default] λ "
     fi
 else
-    echo "#[fg=brightgreen,bg=black]░▒▓#[fg=black,bold,bg=brightgreen] λ "
+    echo "#[fg=green,bg=black]░▒▓#[fg=black,bold,bg=green] λ "
 fi
